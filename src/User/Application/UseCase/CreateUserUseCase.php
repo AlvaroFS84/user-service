0a4d3a08f5ffe3310d68b4c27166eff1a9ed7580
Álvaro <alvarofernandezsalado@gmail.com>
2024-10-20 19:04:28 +0200

    multiple error exception

--- src/User/Application/UseCase/CreateUserUseCase.php
+++ src/User/Application/UseCase/CreateUserUseCase.php
@@ -4,6 +4,8 @@ namespace App\Application\UseCase;
 
 use App\Application\Command\CreateUserCommand;
 use App\Domain\Entity\User;
+use App\Domain\Exception\InvalidParameterException;
+use App\Domain\Exception\MultipleParametersErrorException;
 use App\Domain\Repository\UserRepositoryInterface;
 use App\Domain\ValueObject\Email;
 use App\Domain\ValueObject\Name;
@@ -21,6 +23,43 @@ class CreateUserUseCase
 
     public function __invoke(CreateUserCommand $createUserCommand):void
     {
+        $errors = []; // Array para acumular errores
+
+        try {
+            $name = new Name($createUserCommand->getName());
+        } catch (InvalidParameterException $e) {
+            $errors[] = $e->getMessage();
+        }
+
+        try {
+            $surname = new Surname($createUserCommand->getSurname());
+        } catch (InvalidParameterException $e) {
+            $errors[] = $e->getMessage();
+        }
+
+        try {
+            $email = new Email($createUserCommand->getEmail());
+        } catch (InvalidParameterException $e) {
+            $errors[] = $e->getMessage();
+        }
+
+        try {
+            $password = new Password($createUserCommand->getPassword());
+        } catch (InvalidParameterException $e) {
+            $errors[] = $e->getMessage();
+        }
+
+        if (!empty($errors)) {
+            throw new MultipleParametersErrorException($errors);
+        }
+
+        $user = new User(
+            Uuid::uuid4(),
+            $name,
+            $surname,
+            $email,
+            $password
+        );
         $user = new User(
             Uuid::uuid4(),
             new Name($createUserCommand->getName()),
---
+++ src/User/Domain/Exception/MultipleParametersErrorException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Domain\Exception;
+
+use Exception;
+
+class MultipleParametersErrorException extends Exception
+{
+    
+    
+    public function __construct(private array $errors){
+        parent::__construct(implode(', ', $this->errors));
+    }
+
+}
